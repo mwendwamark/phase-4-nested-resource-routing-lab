1e0e7dedf50e11bc6660e2f2ad413cd7e9068a64
mark mwendwa <mwendwamark310@gmail.com>
2023-06-24 17:05:45 +0300

    completed assignment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,33 @@
 class ItemsController < ApplicationController
-
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
+    render json: items, include: :user,status: :ok
+  end
+
+  def create 
+    item  = Item.create(item_params)
+    render json: item, include: :user, status: :created
+  end
+
+  def show 
+    item = Item.find(params[:id])
+    render json: item, include: :user, status: :ok
+  end
+# Private methods 
+
+  private
+  def item_params 
+    params.permit(:name, :description, :price, :user_id)
+  end
+
+  def render_not_found_response 
+    render json: { error: "Item Not Found"}, status: :not_found
   end
 
 end
